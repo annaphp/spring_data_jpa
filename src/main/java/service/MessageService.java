aae85aef4e922acd7a5acace6ee3dce3d7347dcc
Anna Ilchenko <mymihtav@gmail.com>
2017-04-03 20:55:35 -0700

    comments fixed

--- src/main/java/service/MessageService.java
+++ src/main/java/service/MessageService.java
@@ -24,14 +24,16 @@ public class MessageService {
 	 * throws exception, when proxy sees exception transaction
 	 * is rolled back
 	 */
+
+	/*
+	 * outer transaction succeeds, inner fails
+	 * inner method throws exception and rolls back its own transaction
+	 * it has it's own transaction because of REQUIRES_NEW annotation
+	 * outer transaction succeeds because of try catch block - excepiton was
+	 * handled. Without try catch outer transaction is rolled back too
+     * because exception is thrown
+	 */
 	@Transactional
-	//outer transaction succeeds, inner fails
-   //inner method throws exception and rolls back its own transaction
-	// it has it's own transaction because of REQUIRES_NEW annotation
-	//outer transaction succeeds because of try catch block - excepiton was
-	//handled. Without try catch outer transaction is rolled back too
-	//because exception is thrown
-	
 	public Message save1(Message m){
 		
 		try{
@@ -42,20 +44,23 @@ public class MessageService {
 		return saved;
 	}
 	
-	//inner succeeds outer fails
-	//inner succeeds because exception is not thrown in it, and since
-	//it has its own transaction it executes successfully 
-	//outer fails because of the thrown exception
+	/*
+	 * inner succeeds outer fails
+	 * inner succeeds because exception is not thrown in it, and since
+	 * it has its own transaction it executes successfully 
+	 * outer fails because of the thrown exception 
+	 */
 	@Transactional
      public Message save2(Message m){
 		service2.saveInNewTransaction(new Message("Pupla"), DONT_THROW_X);
-		
 		Message saved = repo.save(m);
 	    throw new RuntimeException();
 	}
-	
-	//outer and inner succeed 
-	//because no exception is thrown
+
+	/*
+	 * outer and inner succeed 
+	 * because no exception is thrown 
+	 */
 	@Transactional
 	public Message save3(Message m){
 	    service2.saveInNewTransaction(new Message("Pupla"), DONT_THROW_X);	
